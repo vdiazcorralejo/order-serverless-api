27c947d3a026d2fbe1ee9a06972bdbb6ee18d67a
Vicente Díaz-Corralejo <vicenxl@hotmail.com>
2026-01-29 13:54:02 +0100

    feat(cognito): enable admin user password auth flow
    Add ALLOW_ADMIN_USER_PASSWORD_AUTH to explicit_auth_flows to enable admin-initiated authentication. This allows using admin-initiate-auth command to obtain IdToken for API Gateway testing.

--- infra/cognito.tf
+++ infra/cognito.tf
@@ -46,6 +46,7 @@ resource "aws_cognito_user_pool_client" "main" {
 
   explicit_auth_flows = [
     "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_ADMIN_USER_PASSWORD_AUTH",
     "ALLOW_REFRESH_TOKEN_AUTH",
     "ALLOW_USER_SRP_AUTH"
   ]
